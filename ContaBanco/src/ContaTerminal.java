7fa582aaed909e33b1304d1892b403ff438f452f
Vinicius <vcanton@gmail.com>
2024-05-08 00:48:16 -0300

    primeiro desafio

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,18 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero = 1021;
+        Float saldo = 237.48F;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = entrada.nextLine();
+        System.out.println("Por favor, digite o número da agência: ");
+        String ag = entrada.nextLine();
+
+        System.out.println("Olá, " + nome+", obrigado por criar uma conta em nosso banco, sua agencia é " + ag+", conta " + numero + " e seu saldo: " + saldo + " já está disponível para saque.");
+        
     }
 }
